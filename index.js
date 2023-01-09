962cc3445ebc031a9e370e93994c7dd5ba6672af
Harald <haraldcarlsten@yahoo.se>
2023-01-09 11:23:03 +0100

    adds correct path to uploads

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "./public/uploads" });
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+	console.log("FILE UPLOADED", req.file);
+	res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size,
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log("Your app is listening on port " + port);
 });
